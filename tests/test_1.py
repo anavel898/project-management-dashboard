872fe1a6b5529a960bfbdd8a84bb105c1f227857
Ana Velimirovic <ana.velimirovic898@gmail.com>
2024-04-01 10:25:30 +0200

    added more tests

--- tests/test_1.py
+++ tests/test_1.py
@@ -8,32 +8,32 @@ arguments = {"name": "Project 1",
             }
 
 class TestProjectMethods(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        cls.projectInstance = p.Project(1, **arguments)
 
     def test_project_constructor(self):
-        self.assertEqual(1, self.projectInstance.id)
-        self.assertEqual("Project 1", self.projectInstance.name)
-        self.assertEqual(1, self.projectInstance.ownerId)
-        self.assertEqual("toy description 1", self.projectInstance.description)
-        self.assertIsNone(self.projectInstance.logo)
-        self.assertIsNone(self.projectInstance.documents)
-        self.assertIsNone(self.projectInstance.contributors)
+        projectInstance = p.Project(1, **arguments)
+        self.assertEqual(1, projectInstance.id)
+        self.assertEqual("Project 1", projectInstance.name)
+        self.assertEqual(1, projectInstance.ownerId)
+        self.assertEqual("toy description 1", projectInstance.description)
+        self.assertIsNone(projectInstance.logo)
+        self.assertIsNone(projectInstance.documents)
+        self.assertIsNone(projectInstance.contributors)
 
     
     def test_to_dict(self):
-        projectAsDict = self.projectInstance.to_dict()
+        projectInstance = p.Project(1, **arguments)
+        projectAsDict = projectInstance.to_dict()
         expectedDict = arguments.copy()
         expectedDict.update({"id": 1, "logo": None, "documents": None, "contributors": None})
         self.assertDictEqual(projectAsDict, expectedDict)
     
     
     def test_update_attribute(self):
+        projectInstance = p.Project(1, **arguments)
         contributorsList = [2, 4]
-        self.projectInstance.update_attribute("contributors", contributorsList)
-        self.assertEqual([2, 4], self.projectInstance.contributors)
-        projectAsDict = self.projectInstance.to_dict()
+        projectInstance.update_attribute("contributors", contributorsList)
+        self.assertEqual([2, 4], projectInstance.contributors)
+        projectAsDict = projectInstance.to_dict()
         expectedDict = arguments.copy()
         expectedDict.update({"id": 1, "logo": None, "documents": None, "contributors": [2, 4]})
         self.assertDictEqual(projectAsDict, expectedDict)
@@ -84,4 +84,12 @@ class TestInMemProjectHandlerMethods(unittest.TestCase):
         self.assertEqual(expected, handlerInstance.get(1))
         self.assertRaises(p.HTTPException, handlerInstance.get, 3)
 
-
+    def test_update_info(self):
+        handlerInstance = p.InMemProjectHandler()
+        handlerInstance.create(**arguments)
+        attributesToUpdate = {"contributors": [2, 4], "description": "new description"}
+        handlerInstance.update_info(1, attributesToUpdate)
+        
+        self.assertEqual([2, 4], handlerInstance.allProjects[1].contributors)
+        self.assertEqual("new description", handlerInstance.allProjects[1].description)
+        self.assertRaises(p.HTTPException, handlerInstance.update_info, 3, attributesToUpdate)

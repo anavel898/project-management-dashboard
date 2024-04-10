4bb337dc760c4a2e390fa90c98fdd7d6fa2c3949
Ana Velimirovic <ana.velimirovic898@gmail.com>
2024-04-10 17:23:41 +0200

    changed db design

--- db/create_db.sql
+++ db/create_db.sql
@@ -1,23 +1,24 @@
 CREATE DATABASE project_manager;
 \c project_manager;
 
-CREATE TABLE projects(
-	id SERIAL PRIMARY KEY,
-	name varchar(100) NOT NULL,
-	created_by integer NOT NULL,
-	created_on timestamp DEFAULT NOW(),
-	description varchar(500) NOT NULL,
-	updated_by integer,
-	updated_on timestamp,
-	logo varchar(2048)
-	);
-
 CREATE TABLE users(
-	id SERIAL PRIMARY KEY,
+	username varchar(10) PRIMARY KEY,
 	name varchar(50) NOT NULL,
 	email varchar(50) NOT NULL,
 	password bytea NOT NULL
 );
+
+CREATE TABLE projects(
+	id SERIAL PRIMARY KEY,
+	name varchar(100) NOT NULL,
+	created_by varchar(10) NOT NULL,
+	created_on timestamp DEFAULT NOW(),
+	description varchar(500) NOT NULL,
+	updated_by integer,
+	updated_on timestamp,
+	logo varchar(2048),
+	FOREIGN KEY (created_by) REFERENCES users(username)
+	);
 
 CREATE TABLE documents(
 	id SERIAL PRIMARY KEY,
@@ -28,9 +29,14 @@ CREATE TABLE documents(
 	FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE
 );
 
+CREATE TYPE role AS ENUM('owner', 'participant');
+
 CREATE TABLE project_access(
 	project_id integer NOT NULL,
-	user_id integer NOT NULL,
+	username varchar(10) NOT NULL,
+	access_type role,
+	is_valid boolean DEFAULT TRUE,
 	FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE,
-	FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+	FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE,
+	PRIMARY KEY (project_id, username)
 );

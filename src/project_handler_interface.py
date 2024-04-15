d33a9ae8ae7ad8fb063fcb6ed13b8e3961bdb094
Ana Velimirovic <ana.velimirovic898@gmail.com>
2024-04-15 12:49:39 +0200

    updating interface

--- src/project_handler_interface.py
+++ src/project_handler_interface.py
@@ -7,7 +7,7 @@ class ProjectHandlerInterface(ABC):
     def create(
         self,
         name: str,
-        createdBy: int,
+        created_by: str,
         description: str
     ) -> None:
         raise NotImplementedError
@@ -17,13 +17,13 @@ class ProjectHandlerInterface(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def get(self, projectId: int) -> object:
+    def get(self, project_id: int) -> object:
         raise NotImplementedError
 
     @abstractmethod
-    def update_info(self, projectId: int, attributesToUpdate: dict) -> object:
+    def update_info(self, project_id: int, attributes_to_update: dict) -> object:
         raise NotImplementedError
     
     @abstractmethod
-    def delete(self, projectId: int) -> None:
+    def delete(self, project_id: int) -> None:
         raise NotImplementedError    

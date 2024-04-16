f25fbb3edca4874e4727b72f97c7ec3df7323d3b
Ana Velimirovic <ana.velimirovic898@gmail.com>
2024-04-16 14:08:14 +0200

    first working version of tests

--- src/routers/project/projects.py
+++ src/routers/project/projects.py
@@ -1,11 +1,23 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from starlette import status
 
+from sqlalchemy.orm import Session
 from src.project_handler_factory import createHandler
 from src.routers.project.schemas import NewProject, UpdatableData, Project
+from src.services.database import SessionLocal, engine
+from src.services.project_manager_tables import Base
 
 router = APIRouter()
 project_handler = createHandler()
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @router.get("/projects", response_model=dict[int, Project])
 async def get_all_projects():
@@ -16,9 +28,10 @@ async def get_all_projects():
 
 
 @router.post("/projects")
-async def make_new_project(new_project: NewProject):
+async def make_new_project(new_project: NewProject, db: Session = Depends(get_db)):
     try:
         project_handler.create(
+            db,
             new_project.name,
             new_project.created_by,
             new_project.description
@@ -28,9 +41,9 @@ async def make_new_project(new_project: NewProject):
         raise ex
 
 @router.get("/project/{project_id}/info", response_model=Project)
-async def get_project_details(project_id: int):
+async def get_project_details(project_id: int, db: Session = Depends(get_db)):
     try:
-        return project_handler.get(project_id)
+        return project_handler.get(db, project_id)
     except HTTPException as ex:
         raise ex
 
--- src/services/db_project_handler.py
+++ src/services/db_project_handler.py
@@ -7,68 +7,70 @@ import os
 from src.routers.project.schemas import Project
 from datetime import datetime
 from fastapi import HTTPException
+from src.services.project_manager_tables import Projects, ProjectAccess, Users, Documents
 
 
-load_dotenv()
-username= os.getenv("DB_USERNAME")
-password=os.getenv("DB_PASSWORD")
-host=os.getenv("DB_HOST")
-port=os.getenv("DB_PORT") 
-db=os.getenv("DB_NAME")
+# load_dotenv()
+# username= os.getenv("DB_USERNAME")
+# password=os.getenv("DB_PASSWORD")
+# host=os.getenv("DB_HOST")
+# port=os.getenv("DB_PORT") 
+# db=os.getenv("DB_NAME")
 
-engine = create_engine(
-    f"postgresql://{username}:{password}@{host}:{port}/{db}")
+# engine = create_engine(
+#     f"postgresql://{username}:{password}@{host}:{port}/{db}")
 
-Base = automap_base()
-# reflect the tables
-Base.prepare(autoload_with=engine)
+# Base = automap_base()
+# # reflect the tables
+# Base.prepare(autoload_with=engine)
 
 # getting mapped classes
-Projects = Base.classes.projects
-Documents = Base.classes.documents
-Users = Base.classes.users
-ProjectAccess = Base.classes.project_access
+# Projects = Base.classes.projects
+# Documents = Base.classes.documents
+# Users = Base.classes.users
+# ProjectAccess = Base.classes.project_access
 
 class DbProjectHandler(ProjectHandlerInterface):
     def create(self,
+               db: Session,
                name: str,
                created_by: str,
                description: str):
-        session = Session(engine)
+        #session = Session(engine)
         # add new project to Projects table
         new_project = Projects(name=name,
                                created_by=created_by,
                                description=description)
-        session.add(new_project)
-        session.commit()
+        db.add(new_project)
+        db.commit()
 
-        # add new permission of type 'owner' to ProjectAccess
-        session.add(ProjectAccess(project_id=new_project.id,
+        #add new permission of type 'owner' to ProjectAccess
+        db.add(ProjectAccess(project_id=new_project.id,
                                   username=created_by,
                                   access_type="owner"))
-        session.commit()
-        session.close()
+        db.commit()
+        db.close()
 
 
-    def get(self, project_id: int):
-        session = Session(engine)
-        project = session.get(Projects, project_id)
+    def get(self, db: Session, project_id: int):
+        #session = Session(engine)
+        project = db.get(Projects, project_id)
         if project is None:
             raise HTTPException(status_code=404,
                                 detail=f"No project with id {project_id} found")
-        docs = session.execute(
+        docs = db.execute(
             select(Documents.id, Documents.name).where(
                 Documents.project_id==project_id
             )
         )
         docs_list = [{"id": row[0], "name": row[1]} for row in docs.all()]
-        contributors = session.execute(
+        contributors = db.execute(
             select(ProjectAccess.username).where(
                 ProjectAccess.project_id == project_id
             )
         )
         contributors_list = [row[0] for row in contributors.all()]
-        session.close()
+        db.close()
         project_repr = Project(id=project.id,
                                name=project.name,
                                created_by=project.created_by,
@@ -83,33 +85,35 @@ class DbProjectHandler(ProjectHandlerInterface):
 
     
     def get_all(self):
-        session = Session(engine)
-        all_project_ids = session.execute(select(Projects.id))
-        all_projects = dict()
-        for row in all_project_ids:
-            curr_project = self.get(row[0])
-            all_projects[row[0]] = curr_project
-        return all_projects
+        # session = Session(engine)
+        # all_project_ids = session.execute(select(Projects.id))
+        # all_projects = dict()
+        # for row in all_project_ids:
+        #     curr_project = self.get(row[0])
+        #     all_projects[row[0]] = curr_project
+        # return all_projects
+        pass
 
     
     def delete(self, project_id: int):
-        session = Session(engine)
-        session.execute(delete(Projects).where(Projects.id == project_id))
-        session.commit()
-        session.close()
+        # session = Session(engine)
+        # session.execute(delete(Projects).where(Projects.id == project_id))
+        # session.commit()
+        # session.close()
+        pass
     
     def update_info(self, project_id: int,
                     attributes_to_update: dict):
-        session = Session(engine)
-        if session.get(Projects, project_id) is None:
-            raise HTTPException(status_code=404,
-                                detail=f"No project with id {project_id} found")
-        attributes_to_update.update({"updated_on": datetime.now()})
+        # session = Session(engine)
+        # if session.get(Projects, project_id) is None:
+        #     raise HTTPException(status_code=404,
+        #                         detail=f"No project with id {project_id} found")
+        # attributes_to_update.update({"updated_on": datetime.now()})
         
-        q = update(Projects).where(Projects.id == project_id).values(
-                attributes_to_update
-        )
-        session.execute(q)
-        session.commit()
-        session.close()
+        # q = update(Projects).where(Projects.id == project_id).values(
+        #         attributes_to_update
+        # )
+        # session.execute(q)
+        # session.commit()
+        # session.close()
         return self.get(project_id)
